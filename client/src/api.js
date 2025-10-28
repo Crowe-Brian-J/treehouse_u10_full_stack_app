e3a266cc4646e52a07e1e68ad22a18f60cce5da3
Brian Crowe <brian.j.crowe@protonmail.com>
2025-10-27 21:45:33 -0400

    implement api.js, remove /src/assets/ directory, file cleanup from vite created .css files

---
+++ client/src/api.js
@@ -0,0 +1,7 @@
+const API_BASE_URL = 'http://localhost:5000/api'
+
+export const getCourses = async () => {
+  const response = await fetch(`${API_BASE_URL}/courses`)
+  if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
+  return await response.json()
+}
